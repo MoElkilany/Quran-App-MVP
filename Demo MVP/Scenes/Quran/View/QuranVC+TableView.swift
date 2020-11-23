4cd4689aeac91ed85bbd55a7f42fc52421f78bfa
mohamed Elkilany <mosayed050@gmail.com>
2020-11-23 17:38:49 +0200

    add tableView Delegation

--- Demo MVP/Scenes/Quran/View/QuranVC+TableView.swift
+++ Demo MVP/Scenes/Quran/View/QuranVC+TableView.swift
@@ -5,4 +5,19 @@
 //  Created by mohamed sayed on 11/23/20.
 //
 
-import Foundation
+import UIKit
+
+extension QuranVC:UITableViewDelegate,UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return presenter.getTableCount()
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: QuranCell.reuseID) as! QuranCell
+        presenter.setCellData(cell: cell, index: indexPath.row)
+        return cell
+        
+    }
+    
+    
+}
